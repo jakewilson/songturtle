0616daa72ffe698f6ba3ec2f72f375949835dc50
Jake Wilson <jakewilsonfl@gmail.com>
2019-10-19 15:51:08 -0400

    Add 0-9 key bindings for seeking.

--- src/index.js
+++ src/index.js
@@ -401,6 +401,19 @@
         }
         break;
       }
+
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        song.seek((e.key / 10) * song.duration);
+        break;
     }
   };
 
