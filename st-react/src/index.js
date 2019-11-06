1104c6777c9b68f66e3a1eaff5e9344db9e9312e
Jake Wilson <jakewilsonfl@gmail.com>
2019-11-05 21:19:59 -0600

    Add font awesome.

--- st-react/src/index.js
+++ st-react/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './css/fa.min.css'; // font awesome
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
